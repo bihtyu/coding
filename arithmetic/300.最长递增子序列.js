26e0a92baef38da7ca5b41ca1a63c994fe124f9c
余振华 <bihtyu@163.com>
2021-10-28 18:50:54 +0800

    add: lc 300

---
+++ arithmetic/300.最长递增子序列.js
@@ -0,0 +1,46 @@
+/*
+ * @lc app=leetcode.cn id=300 lang=javascript
+ *
+ * [300] 最长递增子序列
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var lengthOfLIS = function(nums) {
+  let stack = []
+
+  for (const num of nums) {
+    const top = stack[stack.length - 1]
+    if (top === num) continue
+    if (num > top) {
+      stack.push(num)
+    } else {
+      stack = insert(stack, num)
+    }
+  }
+
+  return stack.length
+};
+
+var insert = function(stack, num) {
+  let left = 0
+  let right = stack.length - 1
+
+  while(left <= right) {
+    const mid = (left + right) >> 1
+    if (num <= stack[mid]) {
+      right = mid - 1
+    } else {
+      left = mid + 1
+    }
+  }
+
+  stack[left] = num
+
+  return stack
+}
+// @lc code=end
+
