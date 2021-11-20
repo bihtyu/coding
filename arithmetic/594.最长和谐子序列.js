f3325aa36ba43974361542c07a7048ae8926fd5a
bihtyu <bihtyu@163.com>
2021-11-20 17:41:26 +0800

    update: lc 594

--- arithmetic/594.最长和谐子序列.js
+++ arithmetic/594.最长和谐子序列.js
@@ -9,19 +9,38 @@
  * @param {number[]} nums
  * @return {number}
  */
- var findLHS = function(nums) {
-  const hash = nums.reduce((obj, num) => {
-    obj[num] = obj[num] ? obj[num] + 1 : 1
-    return obj
-  }, {})
+// reduce
+// var findLHS = function(nums) {
+//   const hash = nums.reduce((obj, num) => {
+//     obj[num] = obj[num] ? obj[num] + 1 : 1
+//     return obj
+//   }, {})
+
+//   let max = 0
+//   Object.keys(hash).forEach(key => {
+//     key = Number(key)
+//     if (hash[key + 1]) {
+//       max = Math.max(max, hash[key] + hash[key + 1])
+//     }
+//   })
+
+//   return max
+// }
+
+// new Map()
+var findLHS = function(nums) {
+  const map = new Map()
+  for (const num of nums) {
+    const cur = map.get(num)
+    cur ? map.set(num, cur + 1) : map.set(num, 1)
+  }
 
   let max = 0
-  Object.keys(hash).forEach(key => {
-    key = Number(key)
-    if (hash[key + 1]) {
-      max = Math.max(max, hash[key] + hash[key + 1])
+  for (const key of map.keys()) {
+    if (map.get(key + 1)) {
+      max = Math.max(max, map.get(key) + map.get(key + 1))
     }
-  })
+  }
 
   return max
 }
