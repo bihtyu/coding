7cd70306cec0a9cfe639b3c4df0ea9224d4d9197
bihtyu <bihtyu@163.com>
2021-11-25 23:25:09 +0800

    add: lc 12

---
+++ arithmetic/12.整数转罗马数字.js
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode.cn id=12 lang=javascript
+ *
+ * [12] 整数转罗马数字
+ */
+
+// @lc code=start
+/**
+ * @param {number} num
+ * @return {string}
+ */
+var intToRoman = function(num) {
+  const basic = {
+    1: 'I',
+    4: 'IV',
+    5: 'V',
+    9: 'IX',
+    10: 'X',
+    40: 'XL',
+    50: 'L',
+    90: 'XC',
+    100: 'C',
+    400: 'CD',
+    500: 'D',
+    900: 'CM',
+    1000: 'M'
+  }
+
+  let res = ''
+
+  const keys = Object.keys(basic)
+  while(num) {
+    const match = keys.filter(key => num >= key).pop()
+    res += basic[match]
+    num -= match
+  }
+
+  return res
+};
+// @lc code=end
